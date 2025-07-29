3ca5698f8330c0e5e7205273667f34d7dcf6769e
ss900371tw <52847269+ss900371tw@users.noreply.github.com>
2025-07-29 17:41:18 +0800

    Update PLAN.py

--- PLAN.py
+++ PLAN.py
@@ -36,7 +36,6 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",  # 明確指定模型
     model_kwargs={"device": device},
     encode_kwargs={"normalize_embeddings": False}
 )
