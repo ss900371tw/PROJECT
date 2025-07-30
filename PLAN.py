ab9e3c35b0a294388e2773dc7a9cdfef0cacf332
ss900371tw <52847269+ss900371tw@users.noreply.github.com>
2025-07-30 16:41:22 +0800

    Update PLAN.py

--- PLAN.py
+++ PLAN.py
@@ -33,7 +33,6 @@ from langchain_community.vectorstores import FAISS
 import torch
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-from langchain_community.embeddings import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",  # ✅ 指定模型
